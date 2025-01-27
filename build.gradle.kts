8ecf61de5fc3089fd3d088403c0531d98cbd4169
Eric Haag <ehaag@gradle.com>
2025-01-26 17:17:16 -0800

    Add `--availability-wait-timeout` when `--fail-if-not-fully-cacheable` is used
    This change adds the `--availability-wait-timeout` option to the build
    scan summary tool invocation with a value of '60' when the
    `--fail-if-not-fully-cacheable` option is used.
    
    This should increase the likelihood of experiment build scans being
    ingested in order to retrieve the required data from the Develocity API
    to determine cacheability.

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,7 +61,7 @@ val isDevelopmentRelease = !hasProperty("finalRelease")
 val releaseVersion = releaseVersion()
 val releaseNotes = releaseNotes()
 val distributionVersion = distributionVersion()
-val buildScanSummaryVersion = "1.0.1-2024.1"
+val buildScanSummaryVersion = "1.0.1-2024.1" //todo update once new version is released
 
 allprojects {
     version = releaseVersion.get()
