5cd988da9a1093dc68dcb4345f5b9db291b131b0
Eric Haag <ehaag@gradle.com>
2025-08-01 15:45:18 -0500

    Fix permissions on scripts within distribution

---
+++ buildSrc/src/main/kotlin/AssembleScripts.kt
@@ -0,0 +1,39 @@
+import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
+import org.gradle.api.tasks.bundling.Zip
+import org.gradle.work.DisableCachingByDefault
+
+@DisableCachingByDefault(because = "Not worth caching")
+abstract class AssembleScripts : Zip() {
+
+    @get:Input
+    abstract val distributionName: Property<String>
+
+    @get:Input
+    abstract val distributionVersion: Property<String>
+
+    @get:InputFiles
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    abstract val distributionContents: ConfigurableFileCollection
+
+    init {
+        archiveBaseName.set(distributionName)
+        archiveFileName.set(distributionName.zip(distributionVersion) { name, version -> "$name-$version.zip" })
+        from(distributionContents) {
+            include("**/*.sh")
+            filePermissions {
+                user {
+                    execute = true
+                }
+            }
+        }
+        from(distributionContents) {
+            exclude("**/*.sh")
+        }
+        into(archiveBaseName)
+    }
+}
